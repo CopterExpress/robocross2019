ace080186c1cdc4ab14ca49d69163b3b20c6b95b
Oleg Kalachev <okalachev@gmail.com>
2019-07-25 19:04:48 +0300

    Improve balloon destroyer

--- src/balloon.py
+++ src/balloon.py
@@ -1,5 +1,8 @@
+print 'initializing...'
+
 import rospy
 import math
+from std_msgs.msg import String
 from geometry_msgs.msg import Vector3Stamped
 from clever import srv
 from std_srvs.srv import Trigger
@@ -27,8 +30,35 @@ land = rospy.ServiceProxy('land', Trigger)
 arming = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
 
 
+def get_distance(x1, y1, z1, x2, y2, z2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+
+
+def get_distance_global(lat1, lon1, lat2, lon2):
+    return math.hypot(lat1 - lat2, lon1 - lon2) * 1.113195e5
+
+
+def navigate_wait(x, y, z, speed, frame_id, tolerance=0.2):
+    navigate(x=x, y=y, z=z, speed=speed, frame_id=frame_id)
+
+    while True:
+        telem = get_telemetry(frame_id=frame_id)
+        if get_distance(x, y, z, telem.x, telem.y, telem.z) < tolerance:
+            break
+        rospy.sleep(0.2)
+
+
+def navigate_global_wait(lat, lon, alt, yaw=float('nan'), speed=2, tolerance=2):
+    navigate_global(lat=lat, lon=lon, z=start.z+alt, yaw=yaw, speed=speed)
+    while not rospy.is_shutdown():
+        telem = get_telemetry()
+        if get_distance_global(lat, lon, telem.lat, telem.lon) < tolerance:
+            return
+        rospy.sleep(0.2)
+
+
 def direction_cb(msg):
-    speed = 3
+    speed = 1.5
     direction = msg
 
     direction = tf_buffer.transform(msg, 'map', rospy.Duration(0.1))
@@ -42,14 +72,37 @@ def direction_cb(msg):
     set_velocity(vx=direction.vector.x, vy=direction.vector.y, vz=direction.vector.z, yaw=yaw)
 
 
-def destroy_target():
-    rospy.Subscriber('red_dead_detection/direction', Vector3Stamped, direction_cb)
-    rospy.spin()
+def destroy_target(time=5):
+    # TODO: make sure not to crash into ground
+    dir_sub = rospy.Subscriber('red_dead_detection/direction', Vector3Stamped, direction_cb)
+    rospy.sleep(time)
+    print 'finish destroying target'
+    dir_sub.unregister()
 
+destroy_start_lat = 56.3346667
+destroy_start_lon = 43.6405074
+destroy_start_yaw = -0.0942823588848
+
+print 'press enter to start'
+raw_input()
 
 print 'take off'
-navigate(z=3, frame_id='body', auto_arm=True, speed=1)
+start = get_telemetry()
+print navigate(z=3, frame_id='body', auto_arm=True, speed=1)
+rospy.sleep(6)
+
+print 'go to destroy start'
+navigate_global_wait(destroy_start_lat, destroy_start_lon, alt=3, yaw=destroy_start_yaw, speed=1)
+
 rospy.sleep(6)
 
 print 'destroy target'
 destroy_target()
+
+print 'fly home'
+navigate_wait(x=start.x, y=start.y, z=start.z+3, frame_id='map', speed=1, tolerance=3)
+
+rospy.sleep(5)
+
+print 'land'
+land()
